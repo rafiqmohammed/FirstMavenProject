9a9a05d055540dabfcb1e754d81f98c86e2a6788
Rafiq Ahmed <mohammed.rafiq.ahmed@accenture.com>
2017-10-12 12:54:33 +0530

    Maven project commit to GITHUB

---
+++ src/main/java/Testing_Oct/TestingCignaDev/App.java
@@ -0,0 +1,13 @@
+package Testing_Oct.TestingCignaDev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
